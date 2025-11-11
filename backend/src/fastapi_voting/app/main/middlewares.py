80cf85f0c78ec87870e74ddd8e1d81b2ff7ac49c
Lyric <vertiipavel0684@gmail.com>
2025-11-11 09:22:13 +0300

    Middleware: Описал Middleware для логирования всего входящего трафика

--- backend/src/fastapi_voting/app/main/middlewares.py
+++ backend/src/fastapi_voting/app/main/middlewares.py
@@ -2,16 +2,36 @@ from fastapi import FastAPI, Request
 
 from fastapi.middleware.cors import CORSMiddleware
 
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from src.fastapi_voting.app.di.dependencies.logging_di import LoggingExceptionDI
+
 
 # --- Конфигурация обработчиков ---
 origins = [
     "https://localhost:5173"
 ]
 
+# --- Пользовательские обработчики ---
+class LogRequestMiddleware(BaseHTTPMiddleware):
+    """Middleware для логирования подробностей входящих запросов."""
 
+    def __init__(self, app: FastAPI):
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next):
+        # --- Внедрение зависимости и логирование ---
+        logger = LoggingExceptionDI("HTTP")(request=request)
+        logger.info(detail="")
+
+        # --- Ответ ---
+        response = await call_next(request)
+        return response
+
+
+# --- Регистрация промежуточных обработчиков ---
 def setup_middlewares(app: FastAPI):
 
-    # --- Регистрация промежуточных обработчиков ---
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
@@ -20,3 +40,5 @@ def setup_middlewares(app: FastAPI):
         allow_headers=["Authorization", "Content-Type", "X-CSRF-Token"],
         expose_headers=["X-CSRF-Token", "WWW-Authenticate"],
     )
+    app.add_middleware(LogRequestMiddleware)
+
